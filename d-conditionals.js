3c2f44aeabbd9e727144b020d2d522078b2ed569
TinekeV <tinekevlieger@gmail.com>
2021-01-12 16:15:47 +0100

    exercise-4-completed

--- d-conditionals.js
+++ d-conditionals.js
@@ -1,17 +1,24 @@
 // ==========================================
 // 1. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 groter dan 20?" in op        // geeft false
 // ==========================================
-
+const numberOne = 10;
+const greaterThan = numberOne > 20;
+console.log(greaterThan)
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 ongelijk aan 11?" in op        // geeft true
 // ==========================================
 
+const numberTwo = 10;
+const unequalTo = numberTwo !== 11;
+console.log(unequalTo)
 
 // ==========================================
 // 3. Declareer een variabele en sla daar in op of de woorden 'zoet' en 'sappig' hetzelfde zijn                // geeft false
 // ==========================================
 
+const tastes = "zoet" === "sappig";
+console.log(tastes)
 
 // ==========================================
 // 4a. Declareer een variabele en sla daar het rapportcijfer 9 in op.
@@ -19,6 +26,10 @@
 //     Als dat zo is, loggen we 'Cum laude!' in de console.                                                     // geeft 'Cum laude!'
 // ==========================================
 
+const grade = 9;
+if (grade > 8) {
+    console.log('Cum laude!')
+}
 
 // ==========================================
 // 5a. Gebruik onderstaande variabelen isRaining en temperature;
@@ -26,3 +37,12 @@
 // 5c. Log "Doe een jas aan!" Als het regent OF kouder is dan 12 graden
 // Tip: verander de waarde van isRaining en temperature even handmatig om te kijken of jouw 2e conditie klopt
 // ==========================================
+
+const isRaining = false;
+const temperature = 8;
+if (isRaining === false && temperature > 21) {
+    console.log("Je kunt een korte broek aan!")
+}
+if (isRaining !== false || temperature < 10) {
+    console.log("Doe een jas aan!")
+}
